b076145acd180bade85895755152915a22080797
JDhyeok <jdhyeok97@gmail.com>
2021-03-24 00:42:19 +0900

    chore: add buttons at home

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import logo from './logo.svg'
+import { Link, Route, Router, Switch } from "react-router-dom";
 import './App.css';
+import { history } from "./history";
+
+import Apply from "./components/Apply";
+import Register from "./components/Register";
+
 
 function App() {
   return (
-    <div className="App">
-      <div className="black-nav">
-        <div> Chicago 졸업작품 </div>
+    <Router history={history}>
+      <div className="App">
+        <div className="black-nav">
+         <div> Chicago 졸업작품 </div>
+        </div>
+        <div className="box">
+          <Link className="authbox" to={"/register"}>
+            채용하기
+          </Link>
+          <Link className="authbox" to={"/apply"}>
+            지원하기
+          </Link>
+        </div>
       </div>
-      <h4> 채용하기    |    지원하기 </h4>
-    </div>
+      <div>
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/apply" component={Apply} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 

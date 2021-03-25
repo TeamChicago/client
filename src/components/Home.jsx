e7371af2e43dd388425bf14170533c417cf08bed
JDhyeok <jdhyeok97@gmail.com>
2021-03-25 16:19:19 +0900

    chore: add descriptions on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,7 +11,11 @@ function Home() {
           <img  src={cbnu} />
         </div>
         <div className="descriptions">
-          <h1> 신한솔 | 장동혁 | 황세호 </h1>
+          <h2>프로젝트 설명</h2>
+          <h4>tesseract-ocr을 통해 이력서에서 텍스트 추출하고 인재추천을 해주는 시스템을 개발하였습니다</h4>
+          <h4>추가적인 설명~~~~~~</h4>
+          <h3 style={{marginTop: 200}}>개발진</h3>
+          <h4> 신한솔 | 장동혁 | 황세호 </h4>
         </div>
       </div>
   )

028c520a5f4bfbff2e9ba54870b28b02a9b5760a
JDhyeok <jdhyeok97@gmail.com>
2021-03-25 16:10:45 +0900

    chore: add cbnu logo on home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,18 @@
+import "./Home.css";
+
 import React from 'react';
+import cbnu from '../images/cbnu.jpg'
 
 function Home() {
 
   return(
       <div>
-        <h1> 충북대학교 로고, 개발자 이름, 간단한 사용 방법 나열 </h1>
+        <div className="cbnu-img">
+          <img  src={cbnu} />
+        </div>
+        <div className="descriptions">
+          <h1> 신한솔 | 장동혁 | 황세호 </h1>
+        </div>
       </div>
   )
 }

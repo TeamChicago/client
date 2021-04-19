927492b875737891fb14a65bdd79823575e27cee
hansolShin <hansolstar@naver.com>
2021-04-19 23:21:37 +0900

    chore: postnews 화면 초안

--- src/components/PostNews.jsx
+++ src/components/PostNews.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import './PostNews.css';
+
 
 function PostNews() {
 
   return (
-
-    <h1> 뉴스 올리기 (모달로 할 예정) </h1>
-
-  )
+    <div className="PostNews">
+      <h1>News Posting</h1>
+      <div>
+        <input className="title-input" type='text' placeholder='제목' />
+      </div>
+      <div className='form-wrapper'>
+        <textarea className="news-area" placeholder='내용'></textarea>
+      </div>
+      <button className="submit-button"> 뉴스 게시하기 </button>
+    </div>
+  );
 }
 
 export default PostNews;

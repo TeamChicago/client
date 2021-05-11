c1357bf7b3a34a57af8c8bb57c1c1d38fd4416b9
hansolShin <hansolstar@naver.com>
2021-05-11 14:26:50 +0900

    chore : postNews add ChangeValue

--- src/components/PostNews.jsx
+++ src/components/PostNews.jsx
@@ -1,19 +1,38 @@
-import React from 'react';
 import './PostNews.css';
-
+import React, { useState } from "react";
 
 function PostNews() {
+  const [titleValue, setTitleValue] = useState("");
+  const [contentValue, setContentValue] = useState("");
+
+  const handleTitleInputChanges = (e) => {
+    setTitleValue(e.target.value);
+    console.log(titleValue);
+  }
+
+  const handleContentInputChanges = (e) => {
+    setContentValue(e.target.value);
+  }
+
+ 
 
   return (
     <div className="PostNews">
       <h1>News Posting</h1>
       <div>
-        <input className="title-input" type='text' placeholder='제목' />
+        <input className="title-input"
+          type='text'
+          value={titleValue}
+          onChange = {handleTitleInputChanges}
+          placeholder='제목' />
       </div>
       <div className='form-wrapper'>
-        <textarea className="news-area" placeholder='내용'></textarea>
+        <textarea className="news-area"
+          value={contentValue}
+          onChange ={handleContentInputChanges}
+          placeholder='내용'></textarea>
       </div>
-      <button className="submit-button"> 뉴스 게시하기 </button>
+      <button className="submit-button" onClick=""> 뉴스 게시하기 </button>
     </div>
   );
 }

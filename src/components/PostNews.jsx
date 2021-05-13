6cbd9aa80c15d9357536ef3f8289f27068d34e1a
hansolShin <hansolstar@naver.com>
2021-05-13 22:22:25 +0900

    chore : change postNews function to class

--- src/components/PostNews.jsx
+++ src/components/PostNews.jsx
@@ -1,40 +1,52 @@
 import './PostNews.css';
 import React, { useState } from "react";
+import{ Component } from 'react';
 
-function PostNews() {
-  const [titleValue, setTitleValue] = useState("");
-  const [contentValue, setContentValue] = useState("");
-
-  const handleTitleInputChanges = (e) => {
-    setTitleValue(e.target.value);
-    console.log(titleValue);
+class PostNews extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      titleValue : '',
+      contentValue : ''
+    }
   }
 
-  const handleContentInputChanges = (e) => {
-    setContentValue(e.target.value);
+  changeHandler = e => {
+    this.setState({[e.target.name]: e.target.value})
+  }
+  submitHandler = e => {
+    e.preventDefault()
+    console.log(this.state)
   }
 
- 
-
-  return (
-    <div className="PostNews">
+  render() {
+    const {titleValue, contentValue} = this.state
+    return (
+      <div className="PostNews">
       <h1>News Posting</h1>
-      <div>
-        <input className="title-input"
-          type='text'
-          value={titleValue}
-          onChange = {handleTitleInputChanges}
-          placeholder='제목' />
-      </div>
-      <div className='form-wrapper'>
-        <textarea className="news-area"
-          value={contentValue}
-          onChange ={handleContentInputChanges}
-          placeholder='내용'></textarea>
-      </div>
-      <button className="submit-button" onClick=""> 뉴스 게시하기 </button>
+      <form onSubmit={this.submitHandler}>
+        <div>
+          <input className="title-input"
+            type='text'
+            name= "titleValue"
+            value={titleValue}
+            onChange = {this.changeHandler}
+            placeholder='제목' />
+        </div>
+        <div className='form-wrapper'>
+          <textarea className="news-area"
+            name= "contentValue"
+            value={contentValue}
+            onChange ={this.changeHandler}
+            placeholder='내용'></textarea>
+        </div>
+        <button className="submit-button" type="submit"> 뉴스 게시하기 </button>
+      </form>
     </div>
-  );
+    );
+  }
 }
 
 export default PostNews;
+
+
